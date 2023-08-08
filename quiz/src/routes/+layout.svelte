612271420e65f0fd8de7283761f0fef555a50d5a
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-08 21:57:01 +0400

    feat: project init. #KQ-1

---
+++ quiz/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { SvelteUIProvider } from "@svelteuidev/core";
+  import { darkTheme, isDarkTheme, lightTheme } from "$lib/store";
+
+  let themeObserver = lightTheme;
+
+  isDarkTheme.subscribe((value) => {
+    themeObserver = value ? darkTheme : lightTheme;
+  });
+</script>
+
+<SvelteUIProvider {themeObserver} withGlobalStyles>
+  <slot />
+</SvelteUIProvider>
