042ebab5e7d001a3507da0ae06573cdacb707f2b
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-10 21:45:04 +0400

    chore: main page setup NA-{13}

---
+++ quiz/src/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Flex, Text } from '@svelteuidev/core';
+	import Logo from '../ui/svg-components/Logo.svelte';
+</script>
+
+<style>
+
+</style>
--- quiz/src/routes/+page.svelte
+++ quiz/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	import { Center, Flex, Text, Title } from '@svelteuidev/core';
-
-
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<Center>
-<h1>HI</h1>
-</Center>
+<div class="bg-gray-300 custom-padding-x">
+	<Navbar />
+</div>
 
 <style>
-
+	.custom-padding-x{
+		padding-left: 1.37rem;
+		padding-right: 1.25rem;
+	}
 </style>
