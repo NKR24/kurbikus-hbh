797d5c72153e83cbbe3c5f088fdfbfdfe69a9227
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-10 23:01:05 +0400

    chore: navbar markup mobile NA-{13}

--- quiz/src/components/Navbar.svelte
+++ quiz/src/components/Navbar.svelte
@@ -5,13 +5,15 @@
 </script>
 
 <nav>
-	<Flex justify="space-between" align="center">
-		<div><Logo /></div>
-		<div class="border border-black h-16" />
-		<div class="flex-col pt-2 items-center w-40 h-16 text">
-			<p>каркасные домапо</p>
-			<p>технологии с 2010 года.</p>
-			<p>Московская область.</p>
+	<Flex align="center" justify="space-between">
+		<div class="flex items-center justify-center gap-4">
+			<div><Logo /></div>
+			<div class="border border-black h-16" />
+			<div class="flex-col pt-2 items-center w-40 h-16 text">
+				<p>каркасные домапо</p>
+				<p>технологии с 2010 года.</p>
+				<p>Московская область.</p>
+			</div>
 		</div>
 		<BurgerIcon />
 	</Flex>
