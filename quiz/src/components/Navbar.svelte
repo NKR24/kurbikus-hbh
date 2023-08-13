594350a524468c7722e06c9ff55f1ded80f8716f
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-13 14:09:38 +0400

    chore: navbar vert line width fix KQ-13

--- quiz/src/components/Navbar.svelte
+++ quiz/src/components/Navbar.svelte
@@ -2,18 +2,17 @@
 	import { Flex, Text } from '@svelteuidev/core';
 	import Logo from '../ui/svg-components/Logo.svelte';
 	import BurgerIcon from './BurgerIcon.svelte';
-	import '../fonts.css'
+	import '../fonts.css';
 </script>
 
 <nav>
 	<Flex align="center" justify="space-between">
 		<div class="flex items-center justify-center gap-4">
 			<div><Logo /></div>
-			<div class="border border-black h-16" />
+			<div class="border border-black border-r-0 h-16" />
 			<div class="flex items-center w-40 h-8">
 				<p class="text">
-					каркасные домапо финской технологии с 2010 года. 
-Московская область.
+					каркасные домапо финской технологии с 2010 года. Московская область.
 				</p>
 			</div>
 		</div>
