189a7d0ce48c979050ba824f30401ca707269072
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-13 13:40:52 +0400

    chore: add mikro font-family KQ-13

--- quiz/src/components/Navbar.svelte
+++ quiz/src/components/Navbar.svelte
@@ -2,6 +2,7 @@
 	import { Flex, Text } from '@svelteuidev/core';
 	import Logo from '../ui/svg-components/Logo.svelte';
 	import BurgerIcon from './BurgerIcon.svelte';
+	import '../fonts.css'
 </script>
 
 <nav>
@@ -9,8 +10,11 @@
 		<div class="flex items-center justify-center gap-4">
 			<div><Logo /></div>
 			<div class="border border-black h-16" />
-			<div class="flex items-center w-40 h-8 text">
-				<p>каркасные домапо технологии с 2010 года. Московская область.</p>
+			<div class="flex items-center w-40 h-8">
+				<p class="text">
+					каркасные домапо финской технологии с 2010 года. 
+Московская область.
+				</p>
 			</div>
 		</div>
 		<BurgerIcon />
@@ -20,5 +24,6 @@
 <style>
 	.text {
 		font-size: 0.5rem;
+		font-family: mikro;
 	}
 </style>
