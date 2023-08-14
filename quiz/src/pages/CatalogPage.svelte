ee86d7b1934516458bc6f89de47eca8a0fa3e336
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-14 19:29:06 +0400

    chore: CatalogPage initial setup KQ-13

---
+++ quiz/src/pages/CatalogPage.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div class="bg-[#3E3E3E;] sm: pl-[1.37rem] pr-[1.25rem]" >
+    <h2>каталоги домов с ценами</h2>
+</div>
--- quiz/src/routes/+page.svelte
+++ quiz/src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import { Center, Flex, Text, Title } from '@svelteuidev/core';
 	import Navbar from '../components/Navbar.svelte';
 	import WelcomePage from '../pages/WelcomePage.svelte';
+	import CatalogPage from '../pages/CatalogPage.svelte';
 </script>
 
 <div>
 	<WelcomePage/>
+	<CatalogPage/>
 </div>
 
 <style>
