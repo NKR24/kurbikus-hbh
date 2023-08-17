8b05013d64d3c9cac39455d3ff5b00e4bc4eacef
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-17 17:36:07 +0400

    chore: snake layout 4 KQ-13

--- quiz/src/pages/WhyUsPage.svelte
+++ quiz/src/pages/WhyUsPage.svelte
@@ -7,32 +7,115 @@
 		<h2 class="">ПОЧЕМУ МЫ?</h2>
 	</div>
 
-	<h3 class="ml-4">01</h3>
-	<p class="ml-5">СТРОИМ/МОНТАЖ КРУГЛЫЙ ГОД</p>
+	<div class="wrapper">
+		<div class="item item-0">
+			<div class="item-number-0">01</div>
+			<div class="mt-5 ml-5">СТРОИМ/МОНТАЖ КРУГЛЫЙ ГОД</div>
+		</div>
 
-    <h3 class="ml-20">02</h3>
-	<p class="ml-20 w-[15.6875rem]">ЛУЧШИЕ ИННОВАЦИОННЫЕ ТЕХНОЛОГИИ И ГАРАНТИЯ КАЧЕСТВА</p>
-   
-    <h3 class="ml-[10.5rem]">03</h3>
-	<p class="ml-[10.63rem] w-60">ДОСТУПНАЯ СТОИМОСТЬ ЗА СЧЕТ СОБСТВЕННОГО ПРОИЗВОДСТВА</p>
+		<div class="item item-1">
+			<div class="item-number-1">02</div>
+			<div class="mt-5 ml-5">
+				ЛУЧШИЕ ИННОВАЦИОННЫЕ ТЕХНОЛОГИИ И ГАРАНТИЯ КАЧЕСТВА
+			</div>
+		</div>
 
-    <h3 class="ml-[8.94rem]">04</h3>
-	<p class="ml-[8.94rem] w-[7.8125rem]">ГИБКАЯ СИСТЕМА ОПЛАТЫ</p>
-   
-    <h3 class="ml-[3.31rem]">05</h3>
-	<p class="ml-[3.56rem] w-[13.1875rem]">БЕСПЛАТНЫЙ ПОДБОР УЧАСТКА И РАСЧЁТ ФУНДАМЕНТА</p>
+		<div class="item item-2">
+			<div class="item-number-2">03</div>
+			<div class="mt-5 ml-[4.5rem] w-48">
+				ДОСТУПНАЯ СТОИМОСТЬ ЗА СЧЕТ СОБСТВЕННОГО ПРОИЗВОДСТВА
+			</div>
+		</div>
+
+		<div class="item item-3">
+			<div class="item-number-3">04</div>
+			<div class="mt-5 ml-[4.5rem] w-48">ГИБКАЯ СИСТЕМА ОПЛАТЫ</div>
+		</div>
+	</div>
 </div>
 
 <style>
 	.text {
 		font-family: mikro;
 	}
-	h3 {
-		font-size: 3rem;
-		font-weight: 700;
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		width: 375px;
+		height: 550px;
+		position: relative;
 	}
-    p{
-        margin-bottom: 2.81rem;
-        font-size: 0.875rem;
-    }
+
+	.item {
+		height: 100px;
+		border: 1px solid black;
+	}
+
+	.item-0 {
+		border-left: 0;
+		border-bottom: 0;
+		width: 255px;
+		height: 90px;
+		align-self: start;
+		position: relative;
+	}
+
+	.item-1 {
+		border-right: 0;
+		border-bottom: 0;
+		margin-left: 79.5px;
+		width: 175px;
+		align-self: start;
+		height: 150px;
+	}
+
+	.item-2 {
+		border-left: 0;
+		border-bottom: 0;
+		margin-right: 39.5px;
+		width: 255px;
+		height: 130px;
+		align-self: end;
+	}
+
+	.item-3 {
+		margin-left: 30px;
+		border-right: 0;
+		border-bottom: 0;
+		width: 304.5px;
+		align-self: start;
+	}
+
+	.item-number-0 {
+		position: absolute;
+		top: -20px;
+		left: 20px;
+		font-size: 3rem;
+		background-color: #dedede;
+		line-height: 35px;
+	}
+	.item-number-1 {
+		position: absolute;
+		font-size: 3rem;
+		top: 70px;
+		left: 100px;
+		background-color: #dedede;
+		line-height: 35px;
+	}
+	.item-number-2 {
+		position: absolute;
+		font-size: 3rem;
+		top: 220px;
+		left: 150px;
+		background-color: #dedede;
+		line-height: 35px;
+	}
+	.item-number-3 {
+		position: absolute;
+		font-size: 3rem;
+		top:350px;
+		left: 100px;
+		background-color: #dedede;
+		line-height: 35px;
+	}
 </style>
