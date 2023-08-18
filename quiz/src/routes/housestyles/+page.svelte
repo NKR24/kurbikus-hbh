6f97e975c82c9ee5b60eb941f21486b44cbb2fd9
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-18 18:41:03 +0400

    fix: fixed images size on  iPad mini #KQ-3

--- quiz/src/routes/housestyles/+page.svelte
+++ quiz/src/routes/housestyles/+page.svelte
@@ -44,20 +44,31 @@ function handleHouseClick(house) {
     activeHouse = house;
 }
 
+import { onMount } from 'svelte';
 
+  let isIpadMini = false;
 
+  onMount(() => {
+    if (navigator.userAgent.includes('iPad') && navigator.userAgent.includes('Mini')) {
+      isIpadMini = true;
+    }
+  });
 
 </script>
 
-<div class=" h-screen bg-[#DEDEDE] box-border py-4 flex items-center flex-col justify-between">
+<div class=" h-screen bg-[#DEDEDE] box-border py-4 xl:py-8 flex items-center flex-col justify-between">
     <h1 class="text-5xl xl:text-[80px] 2xl:text-[110px] font-bold font-['Mikro-Bold'] lowercase">Стиль дома?</h1>
 
     <div class="flex flex-col items-center lg:flex-row lg:items-end ">
     <div class="flex justify-center  flex-wrap mb-20 max-w-sm md:max-w-md  lg:mb-0 lg:w-[60%] lg:ml-[50px] lg:ml-[160px] xl:ml-[200px] xl:w-[70%] lg:gap-x-4 lg:max-w-[55%] lg:pl-14 lg:justify-end">
     {#each houseStyles as houseStyle}
         <div class="flex items-center  flex-col">
-            <img src={houseStyle.image} alt={houseStyle.image} class=" w-[170px] h-full object-cover md:w-[160px] md:h-full min-[820px]:w-[180px]  lg:w-[200px]  ] xl:w-[310px]  2xl:w-[320px]  min-[1800px]:w-[400px] "/>
-            <a on:click={() => handleHouseClick(houseStyle.id)}  href="#"  class="z-10 text-base md:text-xl	xl:text-[32px] flex text-center items-center w-[114px] h-[35px] lg:w-[157px] lg:h-[46px] xl:w-[180px] xl:h-[60px] justify-center border-[0.5px] border-black border-solid font-['Mikro-Regular'] mt-[-30px] lg:mt-[-60px] {activeHouse === houseStyle.id ? 'text-white bg-black' : ''}">{houseStyle.name}</a>
+            {#if isIpadMini}
+            <img src={houseStyle.image} alt={houseStyle.image} class=" w-[224px] h-full "/>
+            {:else}
+        <img src={houseStyle.image} alt={houseStyle.image} class=" w-[170px] h-full object-cover md:w-[160px] md:h-full min-[820px]:w-[180px]  lg:w-[200px]  ] xl:w-[310px]  2xl:w-[320px]  min-[1800px]:w-[400px] "/>
+        {/if}
+        <a on:click={() => handleHouseClick(houseStyle.id)}  href="#"  class="z-10 text-base md:text-xl	xl:text-[32px] flex text-center items-center w-[114px] h-[35px] lg:w-[157px] lg:h-[46px] xl:w-[180px] xl:h-[60px] justify-center border-[0.5px] border-black border-solid font-['Mikro-Regular'] mt-[-30px] lg:mt-[-60px] {activeHouse === houseStyle.id ? 'text-white bg-black' : ''}">{houseStyle.name}</a>
             </div>
     {/each}
 
