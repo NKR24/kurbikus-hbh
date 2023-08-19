f6d6022d14fb8191e84b4cf17f337cae7e8af03c
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-19 17:48:49 +0400

    bug: black button lowercase KQ-13

--- quiz/src/components/BlackBtn.svelte
+++ quiz/src/components/BlackBtn.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <button
-class="bg-[#000] text-[#fff] w-[14.46569rem] sm: h-[3.25rem] text-[1rem] text"
+class="bg-[#000] text-[#fff] w-[14.46569rem] sm: h-[3.25rem] text-[1rem] text lowercase"
 >{text}</button
 >
 <style>
