447b789a2b69f182ae5c779e6bf4ddec732b1799
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-22 21:55:19 +0400

    fix: resolved responsive design problems #KQ-7

--- quiz/src/routes/garage/+page.svelte
+++ quiz/src/routes/garage/+page.svelte
@@ -23,19 +23,19 @@
 
 <SvelteUIProvider>
 	<Center
-		class="flex justify-between items-center h-screen flex-col py-14 md:py-4 lg:py-6 xl:py-8 bg-[#DEDEDE]"
+		class="flex justify-between items-center  h-screen flex-col py-4 md:py-4 2xl:py-14 2xl:pt-[75px] bg-[#DEDEDE]"
 	>
-		<h1 class="text-5xl font-['Mikro-Bold']">Гараж</h1>
+		<h1 class="text-5xl font-['Mikro-Bold'] lg:text-[110px]!">Гараж</h1>
 		<div class="flex flex-col gap-14 items-center">
-            {#if isMobile}
-			<img src={garage} alt="garage" class="" />
-            {:else}
-            <img src={garage_desktop} alt="garage" class="" />
-            {/if}
+			{#if isMobile}
+				<img src={garage} alt="garage" class="image ml-[28px]" />
+			{:else}
+				<img src={garage_desktop} alt="garage" class="image ml-[100px]" />
+			{/if}
 
-			<div class="flex gap-2">
+			<div class="flex gap-2 md:gap-8">
 				<button
-					class="w-[114px] h-[35px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base {selectedInfo ===
+					class="w-[114px] h-[35px] md:w-[171px] md:h-[48px] xl:w-[270px] 2xl:h-[80px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base md:text-xl xl:text-[40px] {selectedInfo ===
 					'с гаражом'
 						? 'text-white bg-black'
 						: ''}"
@@ -46,7 +46,7 @@
 					с гаражом
 				</button>
 				<button
-					class="w-[114px] h-[35px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base {selectedInfo ===
+					class="w-[114px] h-[35px] md:w-[171px] md:h-[48px] xl:w-[270px] 2xl:h-[80px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base md:text-xl xl:text-[40px] {selectedInfo ===
 					'без гаража'
 						? 'text-white bg-black'
 						: ''}"
@@ -57,7 +57,7 @@
 					без гаража
 				</button>
 				<button
-					class="w-[114px] h-[35px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base {selectedInfo ===
+					class="w-[114px] h-[35px] md:w-[171px] md:h-[48px] xl:w-[270px] 2xl:h-[80px] border-[0.5px] border-black border-solid text-black font-['Mikro-Regular'] text-base md:text-xl xl:text-[40px] {selectedInfo ===
 					'выбрать всё'
 						? 'text-white bg-black'
 						: ''}"
@@ -75,9 +75,36 @@
 </SvelteUIProvider>
 
 <style>
+	@media (min-width: 800px) and (min-height: 600px) {
+		h1 {
+			font-size: 30px;
+		}
+
+		.image {
+			width: 320px;
+		}
+	}
 	@media (min-width: 820px) and (min-height: 1180px) {
 		h1 {
 			font-size: 64px;
+		}
+		.image {
+			width: 500px;
+		}
+	}
+
+	@media (min-width: 912px) and (min-height: 1368px) {
+		h1 {
+			font-size: 64px;
+		}
+		.image {
+			width: 500px;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		h1 {
+			font-size: 110px;
 		}
 	}
 </style>
