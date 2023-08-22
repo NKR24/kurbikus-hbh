8c4abb82960e97f4b3d56a688c87b6d13e439416
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-22 21:28:03 +0400

    two thumbs on a track

--- quiz/src/routes/square/+page.svelte
+++ quiz/src/routes/square/+page.svelte
@@ -1,15 +1,20 @@
 <script>
 	import '../../global.css';
-	/**
-	 * @param {{ target: any; }} event
-	 */
-	function updateGradient(event) {
-		const inputElement = event.target;
-		const value =
-			((inputElement.value - inputElement.min) /
-				(inputElement.max - inputElement.min)) *
-			100;
-		inputElement.style.background = `linear-gradient(to right, transparent 0%, transparent ${value}%, black ${value}%, black 100%)`;
+
+	let sliderOne;
+	let sliderTwo;
+	let minGap = 0;
+
+	function slideOne() {
+		if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+			sliderOne.value = parseInt(sliderTwo.value) - minGap;
+		}
+	}
+
+	function slideTwo() {
+		if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+			sliderTwo.value = parseInt(sliderOne.value) + minGap;
+		}
 	}
 </script>
 
@@ -17,27 +22,35 @@
 	<h1 class="text-black text-5xl font-[Mikro-Bold] font-bold mt-[45px]">
 		площадь?
 	</h1>
-	<div class="mt-[137px] flex gap-6">
-		<h1 class="text-2xl font-normal font-[Mikro-Regular]">от 105</h1>
-		<h1 class="text-2xl font-normal font-[Mikro-Regular]">до 250</h1>
+	<div class="values mt-[137px] flex gap-6">
+		<h1 id="range1" class="text-2xl font-normal font-[Mikro-Regular]">от</h1>
+		<h1 id="range2" class="text-2xl font-normal font-[Mikro-Regular]">до</h1>
 	</div>
-	<div class="mt-[57px] relative flex items-center w-full">
-		<hr class="border-black h-[1px] rotate-90 w-[10px] absolute left-[-5px]" />
-		<hr class="border-black h-[1px] rotate-90 w-[10px] absolute left-[24.5%]" />
-		<hr class="border-black h-[1px] rotate-90 w-[10px] absolute left-[48.5%]" />
-		<hr class="border-black h-[1px] rotate-90 w-[10px] absolute left-[72%]" />
-		<hr class="border-black h-[1px] rotate-90 w-[10px] absolute left-[98%]" />
-		<hr class="border-black h-[1px] w-full absolute" />
 
-		<input
-			type="range"
-			min="45"
-			max="250"
-			value="105"
-			class="appearance-none w-full h-[6px] cursor-pointer custom-range bg-gradient"
-			on:input={updateGradient}
-		/>
+	<div class="wrapper">
+		<div class="container">
+			<div class="slider-track" />
+			<input
+				type="range"
+				min="45"
+				max="250"
+				value="105"
+				id="slider-1"
+				bind:this={sliderOne}
+				on:input={slideOne}
+			/>
+			<input
+				type="range"
+				min="45"
+				max="250"
+				value="120"
+				id="slider-2"
+				bind:this={sliderTwo}
+				on:input={slideTwo}
+			/>
+		</div>
 	</div>
+
 	<button
 		class="mt-[196px] w-[141px] h-[35px] bg-black text-[#DEDEDE] text-base"
 	>
@@ -46,18 +59,78 @@
 </div>
 
 <style>
-	.bg-gradient {
-		background: black;
+	.wrapper {
+		position: relative;
+		width: 100%;
 	}
-	.custom-range::-webkit-slider-thumb {
+
+	.container {
+		position: relative;
+		width: 100%;
+		height: 100px;
+		border: 1px solid black;
+	}
+	input[type='range'] {
 		-webkit-appearance: none;
+		-moz-appearance: none;
 		appearance: none;
-		width: 12px;
+		width: 100%;
+		outline: none;
+		position: absolute;
+		margin: auto;
+		top: 0;
+		bottom: 0;
+		background-color: transparent;
+		pointer-events: none;
+	}
+
+	.slider-track {
+		width: 100%;
+		height: 6px;
+		background-color: red;
+		position: absolute;
+		margin: auto;
+		top: 0;
+		bottom: 0;
+		border-radius: 5px;
+	}
+
+	input[type='range']::-webkit-slider-runnable-track {
+		-webkit-appearance: none;
+		height: 5px;
+	}
+	input[type='range']::-moz-range-track {
+		-moz-appearance: none;
+		height: 5px;
+	}
+	input[type='range']::-ms-track {
+		appearance: none;
+		height: 5px;
+	}
+	input[type='range']::-webkit-slider-thumb {
+		-webkit-appearance: none;
 		height: 12px;
+		width: 12px;
 		background-color: #000;
-		border: none;
-		border-radius: 50%;
 		cursor: pointer;
-		z-index: 2;
+		border-radius: 50%;
+		pointer-events: auto;
+		margin-top: -3px;
+	}
+	input[type='range']::-moz-range-thumb {
+		-webkit-appearance: none;
+		height: 12px;
+		width: 12px;
+		cursor: pointer;
+		border-radius: 50%;
+		background-color: black;
+	}
+	input[type='range']::-ms-thumb {
+		-webkit-appearance: none;
+		height: 12px;
+		width: 12px;
+		cursor: pointer;
+		border-radius: 50%;
+		background-color: black;
 	}
 </style>
