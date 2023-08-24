0dd5c87b44442cbb14c3a405304d12c70b789b79
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-24 14:10:44 +0400

    feat: remove files

--- quiz/src/lib/components/QuizTitle.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let content: string;
-</script>
-
-<h1
-	class="text-black text-5xl xl:text-[5rem] 2xl:text-[6.875rem] font-bold font-['Mikro-Bold'] lowercase"
->
-	{content}
-</h1>
