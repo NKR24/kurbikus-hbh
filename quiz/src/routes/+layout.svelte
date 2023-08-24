6d3792a0452f961e122c87d6e18e9f8acad328dd
Nikita Khvorostin <khvorostinn@gmail.com>
2023-08-24 14:09:33 +0400

    feat: format code

--- quiz/src/routes/+layout.svelte
+++ quiz/src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-  import { SvelteUIProvider } from "@svelteuidev/core";
-  import { darkTheme, isDarkTheme, lightTheme } from "$lib/store";
+	import { SvelteUIProvider } from '@svelteuidev/core';
+	import { darkTheme, isDarkTheme, lightTheme } from '$lib/store';
 
-  let themeObserver = lightTheme;
+	let themeObserver = lightTheme;
 
-  isDarkTheme.subscribe((value) => {
-    themeObserver = value ? darkTheme : lightTheme;
-  });
+	isDarkTheme.subscribe((value) => {
+		themeObserver = value ? darkTheme : lightTheme;
+	});
 </script>
 
 <SvelteUIProvider {themeObserver} withGlobalStyles>
-  <slot />
+	<slot />
 </SvelteUIProvider>
