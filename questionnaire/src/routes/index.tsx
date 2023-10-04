f05ef9e37e682296fbed73132d8a6577bf414f6f
Nikita Khvorostin <khvorostinn@gmail.com>
2023-10-05 01:43:53 +0300

    Getting started with Section1 routes/index.tsx

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Title } from "solid-start"
 import Navbar from "~/components/navbar"
+import "~/routes/index.css"
 
 export default function Home() {
   return (
@@ -20,6 +21,31 @@ export default function Home() {
 function Section1() {
   return(
     <>
+    <div class="first-section">
+      <div class="onepiece">
+        <div class="barnhouse">
+          <h1>барнхаус</h1>
+          <div class="barndescripton">
+            <h2>В котором можно жить даже на крайнем севере</h2>
+          </div>
+        </div>
+      </div>
+      <div class="twopiece">
+        <div class="descriptiontwo">
+          <h3>на монолитном фундаменте за 60 дней</h3>
+        </div>
+      </div>
+      <div class="threepiece">
+        <div class="descriptionthree">
+          <h4>гарантия 50 лет.</h4>
+        </div>
+      </div>
+      <div class="fourpiece">
+        <div class="descriptionfour">
+          <h4>Ипотека без первоначального взноса</h4>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
