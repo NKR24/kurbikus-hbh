ef56b141c400f3ec532e2eecfa1a3c394168efa5
Nikita Khvorostin <khvorostinn@gmail.com>
2023-11-27 22:41:08 +0300

    SectionOne design has done

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -33,18 +33,18 @@ export default function Home() {
       <Navbar />
       <Section1 />
       <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Section7 />
-      <Section8 />
-      <Section9 />
-      <Accordion />
-      <Section10 />
-      <Section11 />
-      <Map />
-      <Contacts />
+      {/*<Section3 />*/}
+      {/*<Section4 />*/}
+      {/*<Section5 />*/}
+      {/*<Section6 />*/}
+      {/*<Section7 />*/}
+      {/*<Section8 />*/}
+      {/*<Section9 />*/}
+      {/*<Accordion />*/}
+      {/*<Section10 />*/}
+      {/*<Section11 />*/}
+      {/*<Map />*/}
+      {/*<Contacts />*/}
     </>
   )
 }
