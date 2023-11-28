c89e3c0c5c0b1ad733e73611b2be0b10157c73ca
Nikita Khvorostin <khvorostinn@gmail.com>
2023-11-28 22:42:33 +0300

    Undo

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
     <>
       <Title>Опросник</Title>
       <Navbar />
-      {/*<Section1 />*/}
+      <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
