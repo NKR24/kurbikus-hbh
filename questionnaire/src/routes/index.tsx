fd76ae2f4524fe413eb1e92ce2fc05b34ffc47dd
Nikita Khvorostin <khvorostinn@gmail.com>
2024-01-02 01:11:52 +0300

    Refractoring

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -36,17 +36,17 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
+      {/*<Section4 />*/}
       <Section5 />
       <Section6 />
-      <Section7 />
+      {/*<Section7 />
       <Section8 />
       <Section9 />
       <Accordion />
       <Section10 />
       <Section11 />
       <Map />
-      <Contacts />
+      <Contacts />*/}
     </>
   )
 }
