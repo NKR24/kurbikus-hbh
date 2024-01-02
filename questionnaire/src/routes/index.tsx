fae17e53b937c5c54795323b84a0cdd603588e91
Nikita Khvorostin <khvorostinn@gmail.com>
2024-01-02 18:01:27 +0300

    Question layout for S size has reworked

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -12,6 +12,7 @@ import Consultation from "~/components/index/consultation"
 import Accordion from "~/components/index/accordion"
 import Pros from "~/components/index/pros"
 import Aboutus from "~/components/index/Aboutus"
+import Contacts from "~/components/index/contacts"
 
 export default function Home() {
   return (
@@ -21,15 +22,16 @@ export default function Home() {
       <Barnhouse />
       <Catalog />
       <Questions />
-      <Whyus />
+      {/*<Whyus />*/}
       <Foundation />
       <Demonstration />
-      <Interview />
+      {/*<Interview />
       <Excursion />
       <Consultation />
-      <Accordion />
-      <Pros />
-      <Aboutus />
+      <Accordion />*/}
+      {/*<Pros />*/}
+      {/*<Aboutus />
+      <Contacts />*/}
     </>
   )
 }
