6b3d742acb118a84953250987348eecbc00407b2
Nikita Khvorostin <khvorostinn@gmail.com>
2024-01-13 00:02:21 +0300

    Pros has done

--- questionnaire/src/routes/index.tsx
+++ questionnaire/src/routes/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <Excursion />
       <Consultation />
       <Accordion />
-      {/*<Pros />*/}
+      <Pros />
       <Aboutus />
       <Contacts />
     </>
